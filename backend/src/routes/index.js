e73ad2225cd4a4ee95659413d12f3deac04b9240
naiduramesh283 <naiduramesh283@gmail.com>
2023-09-13 18:56:00 +0530

    feat: impletemented the logo upload and get apis as well

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cafeRoutes = require('./cafe');
 const employeeRoutes = require('./employee');
+const logoRoutes = require('./logo')
 
 const router = express.Router();
 
 // Import and use the employee and café routes
 router.use('/cafes', cafeRoutes);
 router.use('/employees', employeeRoutes);
+router.use('/logo', logoRoutes);
 
 module.exports = router;
---
+++ backend/src/routes/logo.js
@@ -0,0 +1,33 @@
+// routes/cafeRoutes.js
+
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const fs = require("fs")
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+// Create a new café
+router.post("/upload", upload.single("logo"), (req, res) => {
+  res.send({fileName: req.file.filename});
+});
+
+// Retrieve all cafés
+router.get('/get/:fileName', (req, res)=>{
+
+     const fileName = req.params.fileName
+     const filePath = "./uploads/"+fileName
+     const src = fs.createReadStream(filePath);
+     src.pipe(res);
+
+});
+
+module.exports = router;
